74dc4aec7f9c0c5ee765fec8e0b7b0a9687f6807
mllangon <mllangon@myuax.com>
2023-12-14 12:00:07 +0100

    1.2 main

--- Estudinate_c.cpp
+++ Estudinate_c.cpp
@@ -1,13 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
-struct Estudiante{
+struct Estudiante {
     char nombre[50];
     int edad;
     float promedio;
 };
 
-void mostrarEstudiante(const Estudiante& *est) {
+void mostrarEstudiante(const struct Estudiante *est) {
     printf("Nombre: %s\n", est->nombre);
     printf("Edad: %d\n", est->edad);
-    printf("Promedio: %f\n", est->promedio);
+    printf("Promedio: %.2f\n", est->promedio);
+}
+
+int main() {
+    struct Estudiante estudiante;
+
+    printf("Ingrese el nombre del estudiante: ");
+    fgets(estudiante.nombre, 50, stdin);
+
+    printf("Ingrese la edad del estudiante: ");
+    scanf("%d", &estudiante.edad);
+
+    printf("Ingrese el promedio del estudiante: ");
+    scanf("%f", &estudiante.promedio);
+
+    mostrarEstudiante(&estudiante);
+
+    return 0;
 }
