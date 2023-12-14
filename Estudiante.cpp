8f037defa525c7b009757ea1c16c4261bc830bd0
mllangon <mllangon@myuax.com>
2023-12-14 11:48:29 +0100

    1 funcion

--- Estudiante.cpp
+++ Estudiante.cpp
@@ -6,3 +6,10 @@ struct Estudiante{
     int edad;
     float promedio;
 };
+
+void mostrarEstudiante(const Estudiante& estudiante){
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+}
+
