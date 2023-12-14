2f3fb3faf88dcf363e4e3305eaab6586ada1fe5c
mllangon <mllangon@myuax.com>
2023-12-14 13:06:55 +0100

    4 clase

---
+++ 4/Excepciones.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <exception>
+
+class AsistenciaException : public std::exception {
+public:
+    AsistenciaException(const char* message) : msg_(message) {}
+
+    virtual const char* what() const throw () {
+        return msg_.c_str();
+    }
+
+private:
+    std::string msg_;
+};
