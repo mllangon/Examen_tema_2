13828d9561e4602fc44f95853d586c692980da62
mllangon <mllangon@myuax.com>
2023-12-14 13:10:31 +0100

    4 main

---
+++ 4/Excepciones_c.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ERROR_REGISTRO_ASISTENCIA -1
+#define EXITO 0
+
+int funcionRiesgosa() {
+    return ERROR_REGISTRO_ASISTENCIA;
+}
+
+int main() {
+    int resultado = funcionRiesgosa();
+    if (resultado == ERROR_REGISTRO_ASISTENCIA) {
+        fprintf(stderr, "Error al registrar asistencia.\n");
+    }
+
+    return 0;
+}
