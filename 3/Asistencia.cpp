9476b549a7d70e450494e2aec2454421bef1d626
mllangon <mllangon@myuax.com>
2023-12-14 12:58:30 +0100

    3 main

--- 3/Asistencia.cpp
+++ 3/Asistencia.cpp
@@ -23,7 +23,7 @@ struct Estudiante{
     vector<Asistencia> asistencias;
 };
 
-void registrarMateria(Estudiante& est, const string& fecha, const string& materia, estadoAsistencia estado){
+void registrarAsistencia(Estudiante& est, const string& fecha, const string& materia, estadoAsistencia estado){
     Asistencia nuevaAsistencia = {fecha, materia, estado};
     est.asistencias.push_back(nuevaAsistencia);
 }
@@ -37,4 +37,20 @@ void MostrarAsistencia(const Estudiante& est) {
                   << (asistencia.estado == Asistio ? "Asistio" : asistencia.estado == Falta ? "Falta" : asistencia.estado == Justificada ? "Justificada" : asistencia.estado == Tarde ? "Tarde" : "Desconocido")
                   << std::endl;
     }
+}
+
+int main() {
+    Estudiante estudiante;
+
+    cout << "Ingrese el nombre del estudiante: ";
+    getline(cin, estudiante.nombre);
+
+    registrarAsistencia(estudiante, "2021-09-01", "Matematicas", Asistio);
+    registrarAsistencia(estudiante, "2021-09-02", "Matematicas", Falta);
+    registrarAsistencia(estudiante, "2021-09-03", "Matematicas", Justificada);
+    registrarAsistencia(estudiante, "2021-09-04", "Matematicas", Tarde);
+
+    MostrarAsistencia(estudiante);
+
+    return 0;
 }
