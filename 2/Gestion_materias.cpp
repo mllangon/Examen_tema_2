78544c78d8ab87f176ec8d6f09f2389c17328bf7
mllangon <mllangon@myuax.com>
2023-12-14 12:04:19 +0100

    2 estructuras

---
+++ 2/Gestion_materias.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+struct estudiante{
+    std::string nombre;
+    int edad;
+    float promedio;
+    vector<string> materias;
+};
+
+
+
